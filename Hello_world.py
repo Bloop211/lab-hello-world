70aae4717f0416e66025b6508a47ec95e592a6dc
Nikhil <nsrao2640@gmail.com>
2025-07-01 14:20:49 -0500

    Lab day 2 (Hello World)

---
+++ Hello_world.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    print("Hi! Welcome to AI Applications in Computer Science")
+    name = input("Enter your name: ")
+    print(f"Hello {name}.Nice to meet you!")
+
+if __name__ == "__main__":
+    main()
