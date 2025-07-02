89640388abea4cf6d529ecc79b1219f588d562ea
Nikhil <nsrao2640@gmail.com>
2025-07-01 21:01:45 -0500

    Lab day 2 (The Calculator)

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("Hello World")
+firstNum = input("Enter your first number: ")
+secondNum = input("Enter your second number: ")
+whatOp = input("Enter an operation (+, -, *, /): ")
+
+if whatOp == "/" and secondNum == "0":
+    print("Cannot divide by zero!")
+else:
+    answer = eval(firstNum + whatOp + secondNum)
+    print(f"Your answer is: {answer}!")
+    
