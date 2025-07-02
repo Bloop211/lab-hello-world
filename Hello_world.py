e4191cf4b790e97db87c7b71f553d54269a3f6a0
Nikhil <nsrao2640@gmail.com>
2025-07-01 20:15:02 -0500

    Lab day 2 (Hello World + stretch)

--- Hello_world.py
+++ Hello_world.py
@@ -3,7 +3,9 @@
 def main():
     print("Hi! Welcome to AI Applications in Computer Science")
     name = input("Enter your name: ")
-    print(f"Hello {name}.Nice to meet you!")
+    favLang = input("Enter your favorite language: ")
+    print(f"Hello {name}. Nice to meet you!")
+    print(f"{favLang} sounds like a great choice!")
 
 if __name__ == "__main__":
     main()
